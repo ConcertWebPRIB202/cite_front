27c83e95527b51172ae9e4b0ed7b2e74b230d47a
DaHiJl <gmiteor@mail.ru>
2023-12-18 18:08:56 +0300

    Fix photo and hover

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -645,6 +645,9 @@ input::-webkit-inner-spin-button {
 .photo-block{
   justify-content: space-between;
 }
+.login a:hover{
+  color: #F7F4A4;
+}
 @media (max-width: 1440px) {
   .namephoto{
   margin-right: 4%;
@@ -834,7 +837,7 @@ input::-webkit-inner-spin-button {
   }
   .namephoto{
     font-size: 16px;
-    margin-right: 36%;
+    margin-right: 0;
   }
   .tooltip-photo{
     width: 116% !important;
